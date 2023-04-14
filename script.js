6ebff130126bf937fb89dfd9b2aff22c5b481df6
“mehran <mehran.vejdanii@gmail.com>
2023-04-14 21:53:24 +0430

    initSortableList function for replace which item be dragged

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const items = document.querySelectorAll(".item");
+const sortableList = document.querySelector(".sortable-list");
+const items = sortableList.querySelectorAll(".item");
 
 items.forEach((item) => {
   item.addEventListener("dragstart", () => {
@@ -6,3 +7,19 @@ items.forEach((item) => {
   });
   item.addEventListener("dragend", () => item.classList.remove("dragging"));
 });
+
+const initSortableList = (e) => {
+  e.preventDefault();
+  const draggingItem = document.querySelector(".dragging");
+  //getting all items dragging and make arry of them
+  const siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+  //find the sibling after which the dragging item should be placed
+
+  let nextSibling = siblings.find((sibling) => {
+    return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+  });
+  sortableList.insertBefore(draggingItem, nextSibling);
+};
+
+sortableList.addEventListener("dragover", initSortableList);
+sortableList.addEventListener("dragenter", (e) => e.preventDefault());
